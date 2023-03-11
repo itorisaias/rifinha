687e0af909ba2fd29559ba5c461de049d1c8b0e4
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2023-03-11 15:42:53 -0300

    build AppDbContext

---
+++ backend/Rifinha.Api/src/Rifinha.Domain/Models/Entities/v1/Custumer.cs
@@ -0,0 +1,6 @@
+﻿namespace Rifinha.Domain.Models.Entities.v1;
+
+public class Custumer
+{
+    public int Id { get; init;  }
+}
---
+++ backend/Rifinha.Api/src/Rinfinha.Infra/Context/v1/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rifinha.Domain.Models.Entities.v1;
+
+namespace Rinfinha.Infra.Context.v1;
+
+internal class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options): base(options) { }
+
+    public DbSet<Custumer> Custumers  { get; set; }
+}
