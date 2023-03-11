c330e37a2aa26885e56a7451e5e56c6aa0985125
Alexandre Betassa <alexandre.betassa.souza@hotmail.com>
2023-03-11 16:02:23 -0300

    create methods in Database

--- backend/Rifinha.Api/src/Rinfinha.Infra/Database/v1/Database.cs
+++ backend/Rifinha.Api/src/Rinfinha.Infra/Database/v1/Database.cs
@@ -1,30 +1,71 @@
 ﻿using Rifinha.Domain.Models.Contracts.v1;
+using Rinfinha.Infra.Context.v1;
 
 namespace Rinfinha.Infra.Database.v1
 {
     internal class Database<T> : IDatabase<T> where T : class
     {
+        private readonly AppDbContext _db;
+
+        public Database(AppDbContext db) => _db = db;
+
         public Task<T> CreateAsync(T entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = _db.Set<T>().Add(entity);
+                _db.SaveChanges();
+                return Task.FromResult(result.Entity);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Message error: {e.Message}");
+            }
         }
 
         public Task<T> DeleteAsync(T entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = _db.Set<T>().Remove(entity);
+                _db.SaveChanges();
+                return Task.FromResult(result.Entity);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Message error: {e.Message}");
+            }
         }
 
         public Task<T> GetAsync(int id)
         {
+            try
+            {
+                var result = _db.Set<T>().Find(id);
+                return Task.FromResult(result)!;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Message error: {e.Message}");
+            }
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            try
+            {
+                var result = _db.Set<T>().Update(entity);
+                _db.SaveChanges();
+                return Task.FromResult(result.Entity);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Message error: {e.Message}");
+            }
             throw new NotImplementedException();
         }
 
         public Task<T> GetAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<T> UpdateAsync(T entity)
         {
             throw new NotImplementedException();
         }
